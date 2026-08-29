4607dc0722c81c6ac4a71c5495e8e4f1adf756c2
DarkFlameOfficial <mustafamirac000@gmail.com>
2026-08-29 10:43:43 +0300

    App.vue

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<!-- Navigasyon Çubuğuna eklenecek Admin Butonu Örneği -->
+<router-link to="/admin" class="flex flex-col items-center justify-center text-gray-400 hover:text-orange-500 transition py-1">
+  <i class="fa-solid fa-user-shield text-lg"></i>
+  <span class="text-[10px] font-bold mt-1">Admin</span>
+</router-link>
